02372bea529b98c1f24d8f3a8a69f402368c47a9
Petra Krizan <petrakrizan27@gmail.com>
2018-09-05 15:51:09 +0200

    Example client connecting to DPP Data Streams

---
+++ DPPStreamingClientExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace DPPStreamingClientExample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var opts = new IO.Options {ForceNew = true, Upgrade = true};
+      var socket = IO.Socket("https://streams.dapowerplay.com?apikey=xxxxx-xxxx-xxxx-xxxx-xxxxxxxx&apisecret=xxxxx-xxxxxxx-xxxxxxx-xxxxxxx",opts);
+
+      //example parameters
+      var jsonParams =JsonConvert.DeserializeObject("[{ product: 'trades', exchange: 'bitstamp', base: 'BTC', quote: 'USD'}]");
+
+      socket.On(Socket.EVENT_CONNECT, d =>
+      {
+        Console.WriteLine("Connected!");
+        socket.Emit("subscribe", (err, stream) => { }, jsonParams);
+      });
+
+      socket.On("trades", trades => { Console.WriteLine(trades); });
+      Console.ReadLine();
+    }
+  }
+}
